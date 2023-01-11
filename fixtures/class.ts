e6dcc917638997c3ee281317d4c7b7903b3e9bd0
Елизавета <liza@MacBook-Air-Elizaveta.local>
2023-01-11 22:54:04 +0300

    qwe

---
+++ fixtures/class.ts
@@ -0,0 +1,29 @@
+export class registrationForm {
+  public firstName(first: any) {
+    return first;
+  }
+
+  public lastName(last: any) {
+    return last;
+  }
+
+  public patronymic(patronymic: any) {
+    return patronymic;
+  }
+
+  public email(email: any) {
+    return email;
+  }
+
+  public phoneNumber(number: any) {
+    return number;
+  }
+
+  public password(password: any) {
+    return password;
+  }
+
+  public age(age: any) {
+    return age;
+  }
+}
