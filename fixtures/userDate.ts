e70e17cdb16cd23a3b7164fd70abc9a8e68bee46
Елизавета <liza@MacBook-Air-Elizaveta.local>
2023-01-11 23:45:55 +0300

    final_unit

---
+++ fixtures/userDate.ts
@@ -0,0 +1,20 @@
+export enum validUserDate {
+  firstName = "Tina",
+  lastName = "Turner",
+  patronymic = "Vladimirovna",
+  email = "qwe123@gmail.com",
+  phoneNumber = 123456789,
+  password = "qwe123!#",
+  age = 32,
+}
+
+export enum nonValidUserDate {
+  firstName = "11!@",
+  lastName = "11!@",
+  patronymic = "11!@",
+  phoneNumber = "qwe!#$",
+  password = "1111",
+  age = "qwe!#",
+  passOnlyNumbers = "1111111111",
+  passOnlyLetters = "qwertyqwerty",
+}
